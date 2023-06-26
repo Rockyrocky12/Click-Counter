fd524ace4ce656afa09e5eefe193c16bc769201e
Rockyrocky12 <rockyvillafrancaii@gmail.com>
2023-06-26 19:11:17 +0800

    A simple Click Counter

---
+++ main.js
@@ -0,0 +1,9 @@
+
+let score = 0;
+const clickButton = document.getElementById('clickButton');
+const scoreDisplay = document.getElementById('score');
+
+clickButton.addEventListener('click', () => {
+  score++;
+  scoreDisplay.textContent = `Score: ${score}`;
+});
